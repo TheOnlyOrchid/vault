a799e83e2e5f6df4809d252ab41e4b3cd9d4ef9d
Orchid <lokipokes@hotmail.com>
2026-02-07 21:19:33 +0000

    fixed a crach condition found in writeFile, replaced the simple file << content with a chunk-based approach, ensuring even extremely large writes pass with no issues.

--- src/file_utils.cpp
+++ src/file_utils.cpp
@@ -1,13 +1,46 @@
 #include "file_utils.h"
+
+#include <filesystem>
 #include <fstream>
 #include <stdexcept>
 
 void FileUtils::writeFile(const std::string& filename, const std::string& content) {
     std::ofstream file(filename, std::ios::binary);
+
+    // failed to open
     if (!file) {
-        throw std::runtime_error("Failed to write to: " + filename);
+        throw std::runtime_error("Failed to open : " + filename);
     }
-    file << content;
+
+    const char *p = content.data();
+    std::size_t remainingLen = content.size();
+
+    // stack overflow says this has to be const, not constexpr to ensure compatibility with all C++ versions.
+    const std::streamsize streamSizeLimit = std::numeric_limits<std::streamsize>::max();
+    const auto chunk = static_cast<std::size_t>(streamSizeLimit);
+
+    /**
+     * writes in chunks, this prevents a rare crash condition before, where if the size of the data was larger
+     * than the size of the streamsize, it would crash.
+    */
+    while (remainingLen > 0 ) {
+        const std::size_t chunkSize = std::min(remainingLen, chunk);
+        // writes from P chunkSize chars
+        file.write(p, static_cast<std::streamsize>(chunkSize));
+
+        if (!file) {
+            throw std::runtime_error("Failed to write to : " + filename);
+        }
+
+        p += chunkSize;
+        remainingLen -= chunkSize;
+    }
+
+    // this is here so we can have our own error, instead of a standard runtime error.
+    file.flush();
+    if (!file) {
+        throw std::runtime_error("Failed to flush into : " + filename);
+    }
 }
 
 std::string FileUtils::readFile(const std::string& filename) {
@@ -19,7 +52,7 @@ std::string FileUtils::readFile(const std::string& filename) {
         std::istreambuf_iterator<char>());
 }
 
+// this exists as a wrapper to port old code, used to use custom logic.
 bool FileUtils::fileExists(const std::string& filename) {
-    std::ifstream file(filename);
-    return file.good();
+    return std::filesystem::exists(filename);
 }
